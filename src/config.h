1c4ffc1194584251fb3827fca45460160462e70a
FrikkieKritzinger <francinkritzinger@gmail.com>
2026-07-21 17:55:02 +0200

    Initial commit: Project skeleton and architecture

--- src/config.h
+++ src/config.h
@@ -56,8 +56,8 @@
     
     // WiFi configuration (if not already configured)
     // Note: Could also be loaded from EEPROM, but for now hardcoded
-    #define WIFI_SSID               "YourWiFiNetwork"
-    #define WIFI_PASSWORD           "YourPassword"
+    #define WIFI_SSID               "HOMER"
+    #define WIFI_PASSWORD           "20146979"
     
     // Timeouts
     #define ECOS_TIMEOUT            5000            // TCP connection timeout (ms)
